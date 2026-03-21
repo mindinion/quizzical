1d8da6980f1f25903ea070d9ca7c00e7b41517da
mindinion <widdakay@gmail.com>
2026-03-21 21:00:29 +1300

    Add create account files and GroupIcon, fix mysqli migration

---
+++ action-createaccount.php
@@ -0,0 +1,29 @@
+<?php
+	require_once 'dblogin.php';
+	require_once 'security.php';
+
+	// Get inputted details
+	if (isset($_POST['firstname'])) $firstname = sanitizeString($_POST['firstname']);
+	else $firstname = "";
+	if (isset($_POST['lastname'])) $lastname = sanitizeString($_POST['lastname']);
+	else $lastname = "";
+	if (isset($_POST['email'])) $email = sanitizeString($_POST['email']);
+	else $email = "";
+	if (isset($_POST['password'])) $password = sanitizeString($_POST['password']);
+	else $password = "";
+
+	// If the email isn't currently being used, create the account and put them in the public group
+	$resUser = mysqli_query($db_server, "SELECT * FROM Users WHERE email = '" . sanitizeMySQL($email) . "';");
+	$passwordhash = crypt($password, CRYPT_BLOWFISH);
+	if (mysqli_num_rows($resUser) == 0) {
+		mysqli_query($db_server, "INSERT INTO Users (first_name, last_name, email, password_hash, num_results, num_messages, notify_results, notify_message, default_group, timezone)
+		VALUES ('$firstname','$lastname','$email','$passwordhash', 11, 50, 0, 0, 2, 'NZ')");
+		// Find the new user ID and add them to the public group
+		$queryNewId = mysqli_query($db_server, "SELECT id FROM Users WHERE email = '$email';");
+		$newId = mysql_result($queryNewId, 0, 'id');
+		mysqli_query($db_server, "INSERT INTO Memberships (group_id, user_id) VALUES (2, $newId)");
+		header( "Location: index.php?mode=3" );
+	} else {
+		header( "Location: create_account.php?mode=1" );
+	}
+?>
---
+++ create_account.php
@@ -0,0 +1,59 @@
+<html>
+		
+	<head>
+		<title>Quizzical</title>
+		<link href="styles-login.css" type="text/css" rel="stylesheet" />
+		<link rel="icon" type="image/png" href="quizzical_logo.png">
+	</head>
+
+	<body>
+	<div id="newaccountblock">
+		<br>
+		<br> 
+		<div id="center">
+			<span id="TitleText"z>
+				<img src="quizzical_logo.png" id="logo"></img>
+				u i z z i c a l
+			</span>
+		</div>
+		<form method="post" action="action-createaccount.php">
+			<fieldset class="largeform">
+				<h3 id="title">Create Account</h3>
+				<br>	
+				
+				<label for="firstname" class="title">First Name:</label>
+				<input type="text"; class = "field" name="firstname"><br>
+				<br>
+				
+				<label for="lastname" class="title">Last Name:</label>
+				<input type="text"; class = "field" name="lastname"><br>
+				<br>
+				
+				<label for="email" class="title">Email:</label>
+				<input type="text"; class = "field" name="email"><br>
+				<br>
+				
+				<label for="password" class="title">Password:</label>
+				<input type="password"; class = "field" name="password"><br>
+				<br>
+				<div id="title">
+					<input class="submit" type="submit" value="Create Account" id="newaccountbutton"><br><br>
+				</div>	
+				<div id="title">
+					<?php 
+						if (isset($_GET['mode'])) $mode = $_GET['mode'];
+						else $mode = 0;
+						if ($mode == 1) echo "Username already exists";
+					?>
+				</div>
+				<br><br>
+				<div id="title">
+					Site Designed by <a href="mailto:widdakay@gmail.com">Kris Weyling</a> 2013
+				</div>
+				
+			</fieldset>
+		</form>
+	</div>
+	</body>
+	
+</html>
